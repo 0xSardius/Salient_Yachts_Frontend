be65f4740483b76ed573ca7753abf1ba21cc9426
Sardius <0xSardius@gmail.com>
2022-01-11 21:22:26 -0500

    Updated login buttons to connect to Moralis server

--- Salient_Yachts/build/js/main.js
+++ Salient_Yachts/build/js/main.js
@@ -1,3 +1,35 @@
+// Replace with your own Moralis info
+const serverUrl = "**************";
+const appId = "***********";
+Moralis.start({ serverUrl, appId });
+
+
+/* Authentication code */
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
+  async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+  }
+  
+  document.getElementById("btn-login").onclick = login;
+  document.getElementById("btn-logout").onclick = logOut;
+
+
+
+/* Menu slider*/
 const
     arrowDown = Array.from(document.getElementsByClassName('arrow__down')),
     menuItems = Array.from(document.getElementsByClassName('menu__item')),
